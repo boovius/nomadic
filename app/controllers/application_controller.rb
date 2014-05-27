49520e4e0989dbfd2d6b42e00daafe28eefb1e2d
Joshua Book <joshua.book@gmail.com>
2014-05-27 07:28:58 -0700

    app controller and layout config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,39 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter \
+    :expose_user_to_javascript,
+    :expose_config_to_javascript
+
+  after_filter :set_csrf_cookie_for_ng
+
+  helper_method :current_user
+
+  def expose_user_to_javascript
+    gon.jbuilder template: 'app/views/api/users/show.json.jbuilder' if current_user
+  end
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  def expose_config_to_javascript
+    config = {
+      appUrl: APP_CONFIG[:app_url]
+    }
+    gon.config = config
+  end
+
+  protected
+
+  def verified_request?
+    super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
