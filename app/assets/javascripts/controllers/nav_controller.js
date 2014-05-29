dbc54562f77b389bf0d41ba1e4b307f4052f7f0d
Machiko Yasuda <machikoyasuda@gmail.com>
2014-05-28 22:07:56 -0700

    Almost done with OAUTH

--- app/assets/javascripts/controllers/nav_controller.js
+++ app/assets/javascripts/controllers/nav_controller.js
@@ -2,7 +2,44 @@ Nomadic.controller('NavController', ['$scope', function($scope) {
 
   // set facebookAppID
   $scope.facebookAppId = gon.config.facebookAppId
-  console.log($scope.facebookAppId)
+
+  console.log($scope)
+
+  jQuery(function() {
+    $('body').prepend('<div id="fb-root"></div>');
+    return $.ajax({
+      url: "" + window.location.protocol + "//connect.facebook.net/en_US/all.js",
+      dataType: 'script',
+      cache: true
+    });
+  });
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId: gon.config.facebookAppId,
+      cookie: true
+    });
+
+    $('#sign-in').click(function(e) {
+      e.preventDefault();
+      return FB.login(function(response) {
+        if (response.authResponse) {
+          return window.location = '/auth/facebook/callback';
+        }
+      });
+    });
+
+    return $('#sign-out').click(function(e) {
+      FB.getLoginStatus(function(response) {
+        if (response.authResponse) {
+          return FB.logout();
+        }
+      });
+      return true;
+    });
+  };
+
+
 
 
 }])
