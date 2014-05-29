4b7fc3e5de10b6dd2a4ffc20748ed5975e48ae5b
Joshua Book <joshua.book@gmail.com>
2014-05-29 08:01:27 -0700

    mv fb login into own file, log out pieces

---
+++ app/assets/javascripts/controllers/login_test.js
@@ -0,0 +1,39 @@
+console.log(gon);
+
+jQuery(function() {
+  $('body').prepend('<div id="fb-root"></div>');
+  console.log($.ajax({
+    url: "" + window.location.protocol + "//connect.facebook.net/en_US/all.js",
+    dataType: 'script',
+    cache: true
+  }));
+});
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: gon.config.facebookAppId,
+    cookie: true
+  });
+
+  console.log(FB);
+
+  $('#sign-in').click(function(e) {
+    e.preventDefault();
+    FB.login(function(response) {
+      hello = response;
+      console.log(hello);
+      // if (response.authResponse) {
+      //   window.location = '/auth/facebook/callback';
+      // }
+    });
+  });
+
+  $('#sign-out').click(function(e) {
+    FB.getLoginStatus(function(response) {
+      if (response.authResponse) {
+        return FB.logout();
+      }
+    });
+    return true;
+  });
+};
--- app/assets/javascripts/controllers/nav_controller.js
+++ app/assets/javascripts/controllers/nav_controller.js
@@ -1,45 +1,7 @@
-Nomadic.controller('NavController', ['$scope', function($scope) {
+Nomadic.controller('NavController', ['$scope', '$location', function($scope, $location) {
 
   // set facebookAppID
   $scope.facebookAppId = gon.config.facebookAppId
 
-  console.log($scope)
-
-  jQuery(function() {
-    $('body').prepend('<div id="fb-root"></div>');
-    return $.ajax({
-      url: "" + window.location.protocol + "//connect.facebook.net/en_US/all.js",
-      dataType: 'script',
-      cache: true
-    });
-  });
-
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId: gon.config.facebookAppId,
-      cookie: true
-    });
-
-    $('#sign-in').click(function(e) {
-      e.preventDefault();
-      return FB.login(function(response) {
-        if (response.authResponse) {
-          return window.location = '/auth/facebook/callback';
-        }
-      });
-    });
-
-    return $('#sign-out').click(function(e) {
-      FB.getLoginStatus(function(response) {
-        if (response.authResponse) {
-          return FB.logout();
-        }
-      });
-      return true;
-    });
-  };
-
-
-
-
+  // console.log($scope)
 }])
