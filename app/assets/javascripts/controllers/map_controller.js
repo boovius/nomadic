9f071c02763300fad170094e74dda972fefcff53
Machiko Yasuda <machikoyasuda@gmail.com>
2014-06-26 21:40:29 -0700

    Change the default tileLayer

--- app/assets/javascripts/controllers/map_controller.js
+++ app/assets/javascripts/controllers/map_controller.js
@@ -1,7 +1,13 @@
 Nomadic.controller("MapController", [ '$scope', function($scope) {
     angular.extend($scope, {
-        defaults: {
-            scrollWheelZoom: false
-        }
+      center: {
+        lat: 34.0500,
+        lng: -118.2500,
+        zoom: 12
+      },
+      defaults: {
+        scrollWheelZoom: false,
+        tileLayer: "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png"
+      }
     });
 }]);
