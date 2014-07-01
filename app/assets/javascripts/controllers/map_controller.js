cd8fd3970d0eaa4a64e4e328318d084505f05b9c
Joshua Book <joshua.book@gmail.com>
2014-07-01 08:03:42 -0700

    add location getter to map controller

--- app/assets/javascripts/controllers/map_controller.js
+++ app/assets/javascripts/controllers/map_controller.js
@@ -1,7 +1,31 @@
 Nomadic.controller("MapController", [ '$scope', function($scope) {
+
+  $scope.latitude;
+  $scope.longitude;
+
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(setLatLong)
+  }
+  else {
+    alert("Geolcation not supported")
+  }
+
+  function setLatLong(position){
+    $scope.latitude = position.coords.latitude
+    $scope.longitude = position.coords.longitude
+
+    console.log ($scope.latitude)
+    console.log ($scope.longitude)
+
     angular.extend($scope, {
-        defaults: {
-            scrollWheelZoom: false
-        }
+      center: {
+        lat: $scope.latitude,
+        lng: $scope.longitude,
+        zoom: 4
+      },
+      defaults: {
+          scrollWheelZoom: false
+      }
     });
+  }
 }]);
