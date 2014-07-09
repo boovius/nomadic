24583dafd434988dedd8802c61aa0cec6a66bd0f
Machiko Yasuda <machikoyasuda@gmail.com>
2014-07-08 20:11:05 -0700

    Back to basic map

--- app/assets/javascripts/controllers/map_controller.js
+++ app/assets/javascripts/controllers/map_controller.js
@@ -8,28 +8,6 @@ Nomadic.controller("MapController", [ '$scope', function($scope) {
       defaults: {
         scrollWheelZoom: false,
         tileLayer: "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png"
-      },
-      layers: {
-        baselayers: {
-          stamen: {
-            name: 'Stamen',
-            url: "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png",
-            type: 'stamen'
-          }
-        },
-        overlays: {
-          wms: {
-            name: 'EEUU States (WMS)',
-            type: 'wms',
-            visible: true,
-            url: 'http://suite.opengeo.org/geoserver/usa/wms',
-            layerParams: {
-              layers: 'usa:states',
-              format: 'image/png',
-              transparent: true
-            }
-          }
-        }
       }
     });
 }]);
