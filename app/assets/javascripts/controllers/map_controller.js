0e53d6e05610e881063149090e1528155541fcd4
Machiko Yasuda <machikoyasuda@gmail.com>
2014-07-12 18:25:48 -0700

    Haven't yet figured out why geojson is returning 500 error

--- app/assets/javascripts/controllers/map_controller.js
+++ app/assets/javascripts/controllers/map_controller.js
@@ -1,13 +1,30 @@
-Nomadic.controller("MapController", [ '$scope', function($scope) {
+Nomadic.controller("MapController", [ '$scope', '$http', function($scope, $http) {
+  angular.extend($scope, {
+    center: {
+      lat: 34.0500,
+      lng: -118.2500,
+      zoom: 12
+    },
+    defaults: {
+      scrollWheelZoom: false,
+      tileLayer: "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png"
+    }
+  });
+
+  // Get zip code data from a JSON file
+  $http.get("../../../app/assets/javascripts/directives/zip-code-tabulation-areas-2012.geo.json").success(function(data, status) {
     angular.extend($scope, {
-      center: {
-        lat: 34.0500,
-        lng: -118.2500,
-        zoom: 12
-      },
-      defaults: {
-        scrollWheelZoom: false,
-        tileLayer: "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png"
+      geojson: {
+        data: data,
+        style: {
+          fillColor: 'green',
+          weight: 2,
+          opacity: 1,
+          color: 'white',
+          dashArray: '3',
+          fillOpacity: 0.7
+        }
       }
     });
+  });
 }]);
