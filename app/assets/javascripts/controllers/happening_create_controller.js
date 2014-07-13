5e17326cf804fde5b3f30a8006e99cb687fe7883
Joshua Book <joshua.book@gmail.com>
2014-07-12 18:47:24 -0700

    [#74907696] add roving vs stationary to create hap input

--- app/assets/javascripts/controllers/happening_create_controller.js
+++ app/assets/javascripts/controllers/happening_create_controller.js
@@ -1,5 +1,18 @@
 Nomadic.controller('HappeningCreateController', ['$scope', '$window', 'HappeningService', function($scope, $window, HappeningService) {
 
+  $scope.roving = false
+  $scope.stationary = false
+
+  $scope.showRoving = function(){
+    $scope.roving = true
+    $scope.stationary = false
+  }
+
+  $scope.showStationary = function(){
+    $scope.stationary = true
+    $scope.roving = false
+  }
+
   $scope.happening = {}
 
   $scope.createHappening = function(){
